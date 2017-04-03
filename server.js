f140773eda4200985471d6ab4a735c7d4cfed0b8
Ivan Widodo <ivan@wirekom.co.id>
2017-04-03 10:06:42 +0100

    simple fix

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ var Hapi = require('hapi'),
     Routes = require('./routes'),
     config = require('./config'),
     Db = require('./database');
-
+var corsHeaders = require('hapi-cors-headers')
 const server = new Hapi.Server();  
+
+
+
 server.connection({  
     host: config.server.host,
     port: config.server.port
 });
-
+server.ext('onPreResponse', corsHeaders)
 server.route(Routes.endpoints);
 server.start(function() {
     console.log('Server started ', server.info.uri);
